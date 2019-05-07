4c5d8a85229bc06592ea9d61b9ceb634f9797c7d
Pavel Pritchin <p.pritchin@dodopizza.com>
2019-05-07 13:34:54 +0300

    refactor

--- FizzBuzz.Domain/FizzBuzzConverter.cs
+++ FizzBuzz.Domain/FizzBuzzConverter.cs
@@ -4,15 +4,7 @@ namespace FizzBuzz.Domain
 	{
 		public string Convert(int number)
 		{
-			switch (number)
-			{
-				case 1:
-					return "1";
-				case 2:
-					return "2";
-				default:
-					return "4";
-			}
+			return number.ToString();
 		}
 	}
 }
